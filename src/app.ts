7aee5e72d26670265412f794a73614592684de7f
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-04-16 16:13:47 +0200

    typings tsconfig build typescript line styles draw line according to list of walls

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
 /// <reference path="../typings/browser.d.ts" />
-var app;
-(function (app) {
-    var plan = (function () {
-        function plan() {
-            this.width = 800;
-            this.height = 600;
+module app {
+    export class plan {
+        private width = 800;
+        private height = 600;
+        private _room: d3.Selection<any>;
+        constructor() {
             var svg = d3.select('#container')
                 .append('svg')
                 .attr({
-                'width': this.width,
-                'height': this.height
-            });
+                    'width': this.width,
+                    'height': this.height
+                });
             svg.append('rect')
                 .attr({
-                width: this.width,
-                'height': this.height
-            })
+                    width: this.width,
+                    'height': this.height
+                })
                 .style({
-                'fill': '#efefef'
-            });
-            var room = svg.append('g');
+                    'fill': '#efefef'
+                });
+            var room = svg.append('g')
             // .style({
             //     'transform': 'rotate(180deg)',
             //     'transform-origin': 'center'
@@ -30,7 +30,7 @@ var app;
             var yScale = d3.scale.linear()
                 .range([0, 100])
                 .domain([0, 100]);
-            var walls = [
+            var walls: wall[] = [
                 new wall(0, 0, 0, 109.5),
                 new wall(0, 109.5, 32, 109.5),
                 new wall(32, 109.5, 32, 109.5 + 114),
@@ -44,23 +44,30 @@ var app;
                 .classed('wall', true)
                 .append('line')
                 .attr({
-                'x1': function (d) { return xScale(d.x1); },
-                'y1': function (d) { return xScale(d.y1); },
-                'x2': function (d) { return xScale(d.x2); },
-                'y2': function (d) { return xScale(d.y2); }
-            });
+                    'x1': d => xScale(d.x1),
+                    'y1': d => xScale(d.y1),
+                    'x2': d => xScale(d.x2),
+                    'y2': d => xScale(d.y2)
+                });
+
         }
-        return plan;
-    }());
-    app.plan = plan;
-    var wall = (function () {
-        function wall(x1, y1, x2, y2) {
+    }
+
+    class wall {
+        x1: number;
+        y1: number;
+        x2: number;
+        y2: number;
+        constructor(x1: number,
+            y1: number,
+            x2: number,
+            y2: number) {
             this.x1 = x1;
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
         }
-        return wall;
-    }());
-})(app || (app = {}));
+    }
+}
+
 new app.plan();
