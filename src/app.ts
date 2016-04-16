0d8c861c7fcaa3c52c845044ae3b28c515e71bf6
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-04-16 17:51:31 +0200

    Hall added

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,61 @@ module app {
         private _plan: plan;
         constructor(containerId: string) {
             this._plan = new plan(containerId);
-            this.createOffice();
+            var hallPoints = this.createHall();
+            var points = this.createOffice();
+
+            var wall1 = this._plan.addWall(16, 108);
+            var hall = this._plan.createRoom(hallPoints, 'hall', 'yellow');
+            var wall2 = this._plan.addWall(11.5, 133);
+            var office = this._plan.createRoom(points, 'bureau', 'rgba(205,50,155,0.5)');
+
+            var xScale = this._plan.xScale();
+            var yScale = this._plan.yScale();
+
+            var wall1Width = xScale(16);
+            var wall1Height = yScale(108);
+            var hallWidth = xScale(d3.max(hallPoints, p => p[0]));
+            var hallHeight = yScale(d3.max(hallPoints, p => p[1]));
+            var wall2Width = xScale(11.5);
+            var wall2Height = yScale(133);
+            var officeWidth = xScale(d3.max(points, p => p[0]));
+            var officeHeight = yScale(d3.max(points, p => p[1]));
+
+            var totalHeight = officeHeight;
+
+
+            var wall1XTranslate = 0;
+            var wall1YTranslate = totalHeight - wall1Height;
+            wall1.attr('transform', `translate(${wall1XTranslate},${wall1YTranslate})`);
+            
+
+            var hallXTranslate = wall1Width;
+            var hallYTranslate = totalHeight - hallHeight;
+            hall.attr('transform', `translate(${hallXTranslate},${hallYTranslate})`);
+
+            var wall2XTranslate = wall1Width + hallWidth;
+            var wall2YTranslate = totalHeight - wall2Height;
+            wall2.attr('transform', `translate(${wall2XTranslate},${wall2YTranslate})`);
+
+            var officeXTranslate = wall2XTranslate + wall2Width;
+            var officeYTranslate = totalHeight - officeHeight;
+            office.attr('transform', `translate(${officeXTranslate},${officeYTranslate})`);
+
+        }
+        
+        private createHall(): Array<[number, number]> {
+            var points: Array<[number, number]> = [
+                [0, 0],
+                // [16, 0],
+                // [16, 108],
+                [0, 108],
+                [87+16, 108],
+                [87+16, 108 - 133]
+            ];
+            return points;
         }
 
-        private createOffice() {
+        private createOffice(): Array<[number, number]> {
             var y1 = 140;
             var y2 = y1 + 80;
             var y3 = y2 + 135;
@@ -39,8 +90,7 @@ module app {
                 [x12, 0],
                 [0, 0]
             ];
-            this._plan.createRoom(points, 'bureau', 'rgba(205,50,155,0.5)')
-                .attr('transform', 'translate(10,10)');
+            return points;
         }
     }
 }
--- src/plan.ts
+++ src/plan.ts
@@ -6,7 +6,9 @@ module app {
         private _height = 600;
         private _container: d3.Selection<any>;
         private _rooms: Array<any>;
-        
+        private _xScale: d3.scale.Linear<number, number>;
+        private _yScale: d3.scale.Linear<number, number>;
+
         constructor(containerId: string) {
             this._rooms = [];
             this._container = d3.select('#' + containerId)
@@ -26,24 +28,25 @@ module app {
                 .style({
                     'fill': '#efefef'
                 });
+
+            this._xScale = d3.scale.linear()
+                .range([0, 100])
+                .domain([0, 100]);
+            this._yScale = d3.scale.linear()
+                .range([0, 100])
+                .domain([0, 100]);
         }
 
-        public createRoom(points: Array<[number, number]>, name: string, color:string): d3.Selection<any> {
+        public createRoom(points: Array<[number, number]>, name: string, color: string): d3.Selection<any> {
             var room = this._container.append('g')
                 .attr('id', name)
                 .classed('room', true);
-            var xScale = d3.scale.linear()
-                .range([0, 100])
-                .domain([0, 100]);
-            var yScale = d3.scale.linear()
-                .range([0, 100])
-                .domain([0, 100]);
             var xMax = d3.max(points, p => p[0]);
             var yMax = d3.max(points, p => p[1]);
 
             var lineGenerator = d3.svg.line()
-                .x(d => xScale(d[0]))
-                .y(d => yScale(d[1]));
+                .x(d => this._xScale(d[0]))
+                .y(d => this._yScale(d[1]));
             var path = room.append('path')
                 .attr('d', lineGenerator(points))
                 .attr({
@@ -62,8 +65,30 @@ module app {
             this._rooms.push({
                 name: name,
                 points: points
-            })
+            });
+
             return room;
+        }
+
+        public addWall(width: number, height: number): d3.Selection<any> {
+            var wall = this._container.append('g')
+                .classed('wall', true)
+                .append('rect')
+                .attr({
+                    'x': 0,
+                    'y': 0,
+                    'width': this._xScale(width),
+                    'height': this._yScale(height)
+                });
+            return wall;
+        }
+
+        public xScale() {
+            return this._xScale;
+        }
+
+        public yScale() {
+            return this._yScale;
         }
     }
 }
