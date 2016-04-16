3616682a384945552b5ceeb101419b63dca3cb00
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-04-16 12:23:02 +0200

    rectangle with background color

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 (function() {
     'use strict';
     d3.select('#container')
-        .append('svg');    
-    
+        .append('svg').attr({
+            width: '800',
+            'height': '600'
+        })
+        .append('rect')
+        .attr({
+            width: '800',
+            'height': '600'
+        })
+        .style({
+            'fill': '#efefef'
+        });
+
 } ());
