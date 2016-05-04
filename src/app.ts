803e69ea713c16ff39b87ab37a17957331f1cddf
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-05-04 12:48:57 +0200

    Refactoring

--- src/app.ts
+++ src/app.ts
@@ -10,17 +10,24 @@ export class app {
     constructor(containerId: string) {
 
         this._plan = new plan(containerId);
-        
+
         var fc = new furnitureContextual('contextual');
         this._plan.dispatch()
             .on('roomclicked', (d: Iroom) => {
                 fc.update(d.furnitures);
             });
-        
+
         var m = new menu('menu');
+        m.dispatch()
+            .on('itemClick', (d) => {
+                this._plan.render(d.rooms);
+            });
+        
         var service = new dataService();
-        service.read('rooms').then(data => {
-            m.update(data.floors);
-        })
+        service.read('rooms')
+            .then(data => {
+                m.update(data.floors);
+                this._plan.render(data.floors[0].rooms);
+            });
     }
 }
--- src/components/menu.ts
+++ src/components/menu.ts
@@ -1,11 +1,16 @@
 import { Ifloor } from '../models/Ifloor';
+
+interface ImenuDispatch extends d3.Dispatch {
+    itemClick: (d: Ifloor) => void;
+}
 export class menu {
     private _group: d3.Selection<any>;
-    
+    private _dispatch: ImenuDispatch;
     constructor(containerId: string) {
         this._group = d3.select('#' + containerId);
+        this._dispatch = <ImenuDispatch>d3.dispatch('itemClick');
     }
-    
+
     update(floors: Array<Ifloor>): void {
         this._group
             .selectAll('div')
@@ -14,9 +19,13 @@ export class menu {
             .append('div')
             .classed('menu-item', true)
             .on('click', (d, i) => {
-                // this._plan.render(d.rooms);
+                this._dispatch.itemClick(d);
             })
             .text(d => d.name);
     }
-    
+
+
+    dispatch(): d3.Dispatch {
+        return this._dispatch;
+    }
 }
